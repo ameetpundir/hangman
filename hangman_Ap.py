7f49f00a326ec199ae614c6f05ac02a7d0bdb66c
Amit <ameetpundir@gmail.com>
2023-10-04 14:18:06 +0200

    second try!! as in demo

--- hangman_Ap.py
+++ hangman_Ap.py
@@ -1,3 +1,54 @@
 from words import words
+import random
+import string
+# print(words)
 
-print(words)
+
+def validWord(words):
+    ourWord = random.choice(words)
+    while (" " in ourWord or "-" in ourWord):
+        ourWord = random.choice(words)
+    return ourWord
+
+
+def hangman():
+
+    ourWord = validWord(words)
+    print(ourWord)
+    # set of all used words
+    # set of all letter in the word
+
+    allWords = set(string.ascii_lowercase)
+    lettersInWord = set(list(ourWord))
+    wordsUsed = set()
+    lives = 7
+
+    print(lettersInWord)
+
+    # while (len(lettersInWord - wordsUsed) != 0):
+    while (len(lettersInWord) != 0 and lives > 0):
+        currentGuess = [
+            letter if letter in wordsUsed else "-" for letter in ourWord]
+        newLetter = input(f"Input the new letter : {currentGuess}").lower()
+        if (newLetter in allWords):
+            if (newLetter in lettersInWord):
+                lettersInWord.remove(newLetter)
+            elif (newLetter in wordsUsed):
+                print("You have already used this letter, try again!!")
+            else:
+                lives -= 1
+
+            wordsUsed.add(newLetter)
+            print(wordsUsed)
+            print(lettersInWord)
+            print(lives)
+        else:
+            print("Invalid input, please try again!!")
+
+    if (lives == 0):
+        print("Sry!! you lost")
+    else:
+        print(f"Yeyy!! You won, the word is {ourWord}")
+
+
+hangman()
